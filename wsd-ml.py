08e12ee3e57067614d49c36d644e1ffed8f37434
GrayJLG <grayjl3@vcu.edu>
2026-03-24 16:21:44 -0400

    nb, lr, and svc added :)

--- wsd-ml.py
+++ wsd-ml.py
@@ -4,18 +4,27 @@ import sys # for parsing user input
 import re # for regex
 import math # for log functions
 from collections import defaultdict # for bag of words
-from sklearn import MultinomialNB # Multinomial Naive Bayes model https://scikit-learn.org/stable/modules/generated/sklearn.naive_bayes.MultinomialNB.html
+
+# https://scikit-learn.org/stable/modules/generated/sklearn.naive_bayes.MultinomialNB.html
+from sklearn.naive_bayes import MultinomialNB # Multinomial Naive Bayes model 
+
+# https://scikit-learn.org/stable/modules/generated/sklearn.linear_model.LogisticRegression.html#sklearn.linear_model.LogisticRegression
+from sklearn.linear_model import LogisticRegression # logistic regression model
+
+#https://scikit-learn.org/stable/modules/generated/sklearn.svm.LinearSVC.html#sklearn.svm.LinearSVC
+from sklearn.svm import LinearSVC # linear support vector classifier model
+
+#https://scikit-learn.org/stable/modules/generated/sklearn.feature_extraction.text.CountVectorizer.html
+from sklearn.feature_extraction.text import CountVectorizer # for vectorizing text data into numbers that the scikit models can use  
+
 
 # Function for associating a bag of words with a sense for each context in the training data
 def bow(trainText):
 
-    # I found this list https://gist.github.com/sebleier/554280 and decided to pull some common ones from it that would likely appear in the training data. I found that messing
-    # with this did not effect my accuracy or confusion matrix that much so I settled for this. I added the line/lines into here to avoid more regex shenanigans later.
-    stopwords = {"line", "lines", "the", "is", "and", "if", "or", "but", "a", "an", "of", "to", "in", "on", "for", "with", "into"}
+    contexts = []
+    labels = []
 
-    # For storing sense to bag of words associations
-    senseMap = defaultdict(list)
-
+    # Everything from here until the last 'fullContext = ...' statement is identical to my logic from PA3.
     with open (trainText, 'r', encoding ='utf-8') as file:
         instanceBlocks = re.findall(r"<instance.*?</instance>", file.read().lower(), re.DOTALL) # Grabs the raw text from each instance in the training data
 
@@ -25,84 +34,64 @@ def bow(trainText):
         fullContext = re.sub(r"<head>", "", fullContext) # remove the starting head tag(s) around line/lines
         fullContext = re.sub(r"</head>", "", fullContext) # remove the trailing head tag(s) around line/lines
 
-        tokens = set(re.findall(r"\w+", fullContext)) # tokenize the context and place it in a set to remove duplicate entries
-        tokens = tokens - stopwords # remove the stopwords from the set
-            
-        senseMap[sense].append(tokens) # add sense + bag of words associations to the hashmap
+        # Make lists of the contexts and labels to give the model of choice
+        contexts.append(fullContext)
+        labels.append(sense)
 
-    return senseMap
+    return contexts, labels
 
-# Function for creating a decision list from the bag of words associations gathered from the training data
-def dl(senseMap, modelFile):
+# Function for training the naive bayes model
+def nbTrain(contexts, labels):
 
-    decList = [] # List of decision data
-    wordPerSense = {} # Empty dictionary that we will add the amount of times a word shows up given a sense into
-    senseTotals = defaultdict(int) # hashmap for the total amount each sense shows up. Kinda overkill but I'm getting used to working with hashmaps and thought this was easier.
+    # I found this list https://gist.github.com/sebleier/554280 and decided to pull some common ones from it that would likely appear in the training data. I found that messing
+    # with this did not effect my accuracy or confusion matrix that much so I settled for this. I added the line/lines into here to avoid more regex shenanigans later.
+    stopwords = ["line", "lines", "the", "is", "and", "if", "or", "but", "a", "an", "of", "to", "in", "on", "for", "with", "into"]
 
+    #https://scikit-learn.org/stable/modules/generated/sklearn.feature_extraction.text.CountVectorizer.html
+    vec = CountVectorizer(stop_words = stopwords) # this vectorizes our contexts for use in the models, and removes the same stopwords as before
+    wordMat = vec.fit_transform(contexts) # matrix of all of the feature vectors
 
-    # Go through the sense to bag of words hashmap and count how many times each sense appears
-    for sense in senseMap:
-        for tokens in senseMap[sense]:
-            senseTotals[sense] += 1
+    # make the naive bayes model and fit it to our feature vectors and their labels
+    nb = MultinomialNB()
+    nb.fit(wordMat, labels)
 
-            # For every bag of words, add unseen tokens to the hashmap and add to the total amount of times it has been seen with the associated sense            
-            for token in tokens:
-                if token not in wordPerSense:
-                    wordPerSense[token] = defaultdict(int)
-                wordPerSense[token][sense] += 1
+    return nb, vec
 
-    # Build out the decision list for every unique word
-    for token in wordPerSense:
+def lrTrain(contexts, labels):
+    
+    # I found this list https://gist.github.com/sebleier/554280 and decided to pull some common ones from it that would likely appear in the training data. I found that messing
+    # with this did not effect my accuracy or confusion matrix that much so I settled for this. I added the line/lines into here to avoid more regex shenanigans later.
+    stopwords = ["line", "lines", "the", "is", "and", "if", "or", "but", "a", "an", "of", "to", "in", "on", "for", "with", "into"]
 
-        # grab the number of times the word has appeared in the product sense and the phone sense
-        prodCount = wordPerSense[token]["product"]
-        phoneCount = wordPerSense[token]["phone"]
+    #https://scikit-learn.org/stable/modules/generated/sklearn.feature_extraction.text.CountVectorizer.html
+    vec = CountVectorizer(stop_words = stopwords) # this vectorizes our contexts for use in the models, and removes the same stopwords as before
+    wordMat = vec.fit_transform(contexts) # matrix of all of the feature vectors
 
-        # the total amount of times the word appeared in the entire training data
-        totCount = prodCount + phoneCount
+    # make the logistic regression model and fit it to our feature vectors and their labels
+    lr = LogisticRegression(max_iter=1000)
+    lr.fit(wordMat, labels)
 
-        # If the word never appeared with a certain sense, it should weighted to indicate the other sense. I played with these numbers alot but settled on +-3 since 
-        # most predictions that had a count for both were in the range of 1-5 and 3 felt middle of the road enough to not always favor but heavily weight. 
-        if prodCount == 0:
-            logScore = -3 
-            match = "phone"
+    return lr, vec
 
-        elif phoneCount == 0: 
-            logScore = 3
-            match = "product"
+def svcTrain(contexts, labels):
 
-        else:
+    # I found this list https://gist.github.com/sebleier/554280 and decided to pull some common ones from it that would likely appear in the training data. I found that messing
+    # with this did not effect my accuracy or confusion matrix that much so I settled for this. I added the line/lines into here to avoid more regex shenanigans later.
+    stopwords = ["line", "lines", "the", "is", "and", "if", "or", "but", "a", "an", "of", "to", "in", "on", "for", "with", "into"]
 
-            # If there are counts for both, do the monster math using the formula from the slides
-            prodProb = prodCount / totCount
-            phoneProb = phoneCount / totCount
-            logScore = math.log2(prodProb / phoneProb)
+    #https://scikit-learn.org/stable/modules/generated/sklearn.feature_extraction.text.CountVectorizer.html
+    vec = CountVectorizer(stop_words = stopwords) # this vectorizes our contexts for use in the models, and removes the same stopwords as before
+    wordMat = vec.fit_transform(contexts) # matrix of all of the feature vectors
 
-            if(logScore > 0):
-                match = "product"
+    # make the support vector classifier model and fit it to our feature vectors and their labels
+    svc = LinearSVC()
+    svc.fit(wordMat, labels)
 
-            else:
-                match = "phone"
+    return svc, vec
 
-        # Add a tuple of values to the decision list for every unique word we have
-        decList.append((token, logScore, match))
+def modelApply(model, vec, inFile):
 
-    # print(senseTotals) for use in MFS
-
-    # Use lambda function to grab logScore value from each tuple (shoutout 304 lambda functions are my new best friend)
-    decList.sort(key=lambda x: abs(x[1]), reverse=True)    
-
-    # Put results into our model file. 
-    with open(modelFile, 'w', encoding ='utf-8') as file:
-        for token, logScore, sense in decList:
-            file.write(f"{token}\t{logScore}\t{sense}\n")
-
-    return decList
-
-def modelApply(dl, inFile):
-    # Again this is just for removing these words at the end
-    stopwords = {"line", "lines"}
-
+    # Everything from here until the last 'fullContext = ...' statement is identical to my logic from PA3.
     with open(inFile, 'r', encoding='utf-8') as file:
         instanceBlocks = re.findall(r"<instance.*?</instance>", file.read().lower(), re.DOTALL) # grab all the instances in the same way we did last time
     
@@ -113,17 +102,9 @@ def modelApply(dl, inFile):
         fullContext = re.sub(r"<head>", "", fullContext) # remove the head tag
         fullContext = re.sub(r"</head>", "", fullContext) # remove the head tag
 
-        # grab tokens and filter stopwords like in the bow function
-        tokens = set(re.findall(r"\w+", fullContext))
-        tokens = tokens - stopwords
 
-        # Go through the decision list, which has been sorted in order of strongest word association to weakest.
-        # Once we hit a token in our decision list that is also in the test data context tokens, that is 
-        # guarenteed to be the strongest association due to the way our decision list is sorted so we grab the sense and predict it. 
-        for token, logScore, sense in dl:
-            if token in tokens:
-                prediction = sense
-                break
+        wordMat = vec.transform([fullContext]) # make feature vector matrix based on the vector we trained the model on
+        prediction = model.predict(wordMat)[0] # use the feature vector matrix to predict a sense for every feature vector
 
         # prediction = "product" (used for MSF)
 
@@ -135,12 +116,27 @@ def main():
     # grab input files
     trainFile = sys.argv[1] 
     testFile = sys.argv[2]
-    modelFile = sys.argv[3]
 
-    # Do what must be done
-    senseMap = bow(trainFile)
-    trainedModel = dl(senseMap, modelFile)
-    modelApply(trainedModel, testFile)
+    # Grab contexts and labels for bag of words
+    contexts, labels = bow(trainFile)
+
+    # determine desired model, default to naive bayes if none specified
+    if(len(sys.argv) > 3):
+        if(sys.argv[3] == "NaiveBayes"):
+            model, vec = nbTrain(contexts, labels)
+        elif(sys.argv[3] == "LinearRegression"):
+            model, vec = lrTrain(contexts, labels)
+        elif(sys.argv[3] == "SVC"):
+            model, vec = svcTrain(contexts, labels)
+        else:
+            # use sys.stderr so we can print to console, otherwise this gets added to my-line-answers.txt
+            print("Model name not recognized, performing Naive Bayes by default", file=sys.stderr)
+            model, vec = nbTrain(contexts, labels)
+    else:
+        model, vec = nbTrain(contexts, labels)
+    
+    # apply the model to the test data
+    modelApply(model, vec, testFile)
 
 if __name__ == "__main__":
     main()
